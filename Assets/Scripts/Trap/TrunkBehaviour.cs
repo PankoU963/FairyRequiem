c6d11da0c451b9a881b73de1ad21469f029ddf30
Santiago Bravo Pareja <Santiagobp-12@hotmail.com>
2025-07-15 01:37:54 -0500

    escenario mas melo

--- Assets/Scripts/Trap/TrunkBehaviour.cs
+++ Assets/Scripts/Trap/TrunkBehaviour.cs
@@ -6,13 +6,7 @@ public class TrunkBehaviour : MonoBehaviour
 {
     private Vector3 velocidadRotacion = new Vector3(0f, -150f, 0f);
 
-    public float speed = 0.1f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed = 0.04f;
 
     // Update is called once per frame
     void Update()
