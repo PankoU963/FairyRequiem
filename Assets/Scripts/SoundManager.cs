89b2a44aceeff5d53aa2cfc9312d0713f8231807
Santiago Bravo Pareja <Santiagobp-12@hotmail.com>
2025-07-16 00:42:43 -0500

    hpt, estos props eran en ambiente

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public enum SoundType {
+    PASOS,
+    ATAQUE,
+    DAÑO,
+    AMBIENTE,
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] soundList;
+    private static SoundManager instance;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void Playsound(SoundType sound, float volume = 1)
+    {
+        instance.audioSource.PlayOneShot(instance.soundList[(int)sound], volume);
+    }
+
+}
