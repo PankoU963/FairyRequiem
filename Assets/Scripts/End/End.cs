172cea84e2123376e657d27c76e85569f3fc11e0
TheFoolBrave <santiago_bravo82211@elpoli.edu.co>
2025-07-20 21:06:59 -0500

    Scenas finales

---
+++ Assets/Scripts/End/End.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Menu();
+        }
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
